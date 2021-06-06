70febb332d0a5cbdac5aa3308bc6cba9af1f353d
proffix4@gmail.com <proffix4@gmail.com>
2021-06-06 16:42:42 +0600

    Init

---
+++ FictionBook_install.sh
@@ -0,0 +1,4 @@
+WINEARCH=win32 WINEPREFIX=~/.local/share/wineprefixes/win32 wineboot -u
+winetricks prefix=win32 msxml4 msxml6 vcrun2008 ie6
+WINEARCH=win32 WINEPREFIX=~/.local/share/wineprefixes/win32 wine FictionBook.Editor.Release.v2.6.7.build.14.Nov.exe
+WINEPREFIX=~/.local/share/wineprefixes/win32 wine C:\\Program\ Files\\FictionBook\ Editor\\FBE.exe
---
+++ _port/FictionBook Editor/App/FBE/Scripts/18_Встроенный таймер.js
@@ -0,0 +1,11 @@
+﻿// Таймер с напоминанием Forcosigan,  wotti
+
+var answer2="";
+var id="";
+function Run(){
+	var answer = confirm ("Включить напоминание о сохранении?");
+	if (answer)
+	{
+		id = setInterval(function(){answer2 = confirm ("Не забудьте сохраниться!\n\nДля отключения напоминания нажмите \"Отмена\""); if (!answer2) clearInterval(id);}, 300000);
+	}
+}
